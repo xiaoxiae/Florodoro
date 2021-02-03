502887c130943a6dc75083a394c1053dce278c70
xiaoxiae <tomas.slama.131@gmail.com>
2021-02-03 11:31:27 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ script_location = path.abspath(path.dirname(__file__))
 setup(
     # information about the package
     name="florodoro",
-    version="0.1.4",
+    version="0.1.5",
     author="Tomáš Sláma",
     author_email="tomas@slama.dev",
     keywords="education pyqt5 plants pomodoro",
